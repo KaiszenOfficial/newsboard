dcb92a2d9225a516f22e1b3f21b46fd947ec9628
Supratik <supratik1992@gmail.com>
2021-10-02 11:19:34 +0530

    initial commit

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as AppHeader } from './AppHeader/AppHeader';
-export { default as Breadcrumb } from './Breadcrumb/Breadcrumb';
-export { default as Headlines } from './Headlines/Headlines';
-export { default as Loader } from './Loader/Loader';
